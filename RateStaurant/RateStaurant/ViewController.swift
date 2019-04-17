53beafe50c468448fe255e3de331b4f851438c4b
Jakub Kołodziej <kolomakuba@gmail.com>
2019-04-17 20:38:35 +0200

    initial commit

---
+++ RateStaurant/RateStaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RateStaurant
+//
+//  Created by Jakub Kołodziej on 17/04/2019.
+//  Copyright © 2019 Jakub Kołodziej. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
